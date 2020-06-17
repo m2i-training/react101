2c6ddfc274288cd15b61210d0bd43614423ca175
Ahmed <ahhosni@vistaprint.com>
2020-06-17 07:28:31 +0100

    Add game lab

--- demo-1-click-counter/click-counter/src/index.js
+++ demo-1-click-counter/click-counter/src/index.js
@@ -7,7 +7,10 @@ import registerServiceWorker from './registerServiceWorker';
 let model = { clicks: 0 };
 
 function render() {
-    ReactDOM.render(<App clicks={model.clicks} onClick={()=> { model.clicks += 1; render(); }} />, document.getElementById('root'));
+    ReactDOM.render(
+		<App clicks={model.clicks} onClick={()=> { model.clicks += 1; render(); }} />,
+		document.getElementById('root')
+	);
 }
 render();
 registerServiceWorker();
